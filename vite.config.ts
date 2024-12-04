a48a499c7210daf0bd61c78304c279152f244d9a
Welington <wp22santos@gmail.com>
2024-12-04 15:10:40 -0300

    fix: simplify vite and vercel configurations

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
+    emptyOutDir: true,
     rollupOptions: {
       output: {
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.')
-          const ext = info[info.length - 1]
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
-            return `assets/images/[name][extname]`
-          }
-          return `assets/[name][extname]`
-        },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
       }
     }
   },
