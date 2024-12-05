30dce5ed1bc6ab8ad61fbc4f48869c606eb88cc9
Welington <wp22santos@gmail.com>
2024-12-04 23:17:28 -0300

    Update vite config to resolve jspdf module

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,10 @@ export default defineConfig({
   build: {
     assetsDir: 'assets',
     chunkSizeWarningLimit: 1000
+  },
+  resolve: {
+    alias: {
+      'jspdf': resolve(__dirname, 'node_modules/jspdf/dist/jspdf.es.min.js')
+    }
   }
 })
